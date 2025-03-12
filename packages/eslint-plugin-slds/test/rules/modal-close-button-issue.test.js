ea70ae779994959c59bf9fbbfb0bdf0838a6a695
Kishore Nemalipuri <lnemalipuri@salesforce.com>
2025-03-12 17:32:32 +0530

    Adding fixes for modal-close-button rules
    Signed-off-by: Kishore Nemalipuri <lnemalipuri@salesforce.com>

--- packages/eslint-plugin-slds/test/rules/modal-close-button-issue.test.js
+++ packages/eslint-plugin-slds/test/rules/modal-close-button-issue.test.js
@@ -1,6 +1,10 @@
 const rule = require("./../../src/rules/modal-close-button-issue");
 const { RuleTester } = require("eslint");
 
+function normalizeHTML(html) {
+  return html.replace(/\s+/g, ' ').trim();
+}
+
 const ruleTester = new RuleTester({
   parser: require.resolve("@html-eslint/parser"), // Specify the parser for HTML files
 });
@@ -75,5 +79,97 @@ ruleTester.run("slds-modal-button-issue", rule, {
       ],
       output: `<button class="slds-button slds-modal__close"><lightning-icon ></lightning-icon></button>`,
     },
+
+    /** Real use cases from source graph */
+    // Scenario 1
+    {
+      code: `<button class="slds-button slds-button_icon slds-modal__close slds-button_icon-inverse">
+                <lightning-icon
+                    icon-name="utility:close"
+                    alternative-text={cancelButtonLabel}
+                    variant="bare-inverse"
+                    size="large" >
+                </lightning-icon>
+                <span class="slds-assistive-text">{cancelButtonLabel}</span>
+            </button>`,
+      errors: [
+        { messageId: "removeClass"},
+        { messageId: "changeVariant" },
+        { messageId: "removeVariant" }
+      ],
+      output: `<button class="slds-button slds-button_icon slds-modal__close">
+                <lightning-icon
+                    icon-name="utility:close"
+                    alternative-text={cancelButtonLabel}
+                    
+                    size="large" >
+                </lightning-icon>
+                <span class="slds-assistive-text">{cancelButtonLabel}</span>
+            </button>`,
+    },
+
+    // Scenario 2
+    {
+      code: `<lightning-button-icon
+                title={labels.closeButton} icon-name="utility:close"
+                onclick={closeAction}
+                class="slds-button slds-button_icon slds-modal__close slds-button--icon-inverse" variant="bare-inverse" size="large">
+            </lightning-button-icon>`,
+      errors: [
+        { messageId: "removeClass"},
+        { messageId: "changeVariant" }
+      ],
+      output: `<lightning-button-icon
+                title={labels.closeButton} icon-name="utility:close"
+                onclick={closeAction}
+                class="slds-button slds-button_icon slds-modal__close" variant="bare" size="large">
+            </lightning-button-icon>`,
+    },
+    // Scenario 2.a where icon-class exist and that should be treated the same way as class
+    {
+      code: `<lightning-button-icon
+                title={labels.closeButton} icon-name="utility:close"
+                onclick={closeAction}
+                class="slds-button slds-button_icon"
+                icon-class="slds-button slds-button_icon slds-modal__close slds-button--icon-inverse"
+                variant="bare-inverse" size="large">
+            </lightning-button-icon>`,
+      errors: [
+        { messageId: "removeClass"},
+        { messageId: "changeVariant" }
+      ],
+      output: `<lightning-button-icon
+                title={labels.closeButton} icon-name="utility:close"
+                onclick={closeAction}
+                class="slds-button slds-button_icon"
+                icon-class="slds-button slds-button_icon slds-modal__close"
+                variant="bare" size="large">
+            </lightning-button-icon>`,
+    },
+    // Scenario 3
+    {
+      code: `<header class="slds-modal__header">
+                <button class="slds-button slds-button_icon slds-modal__close slds-button_icon-inverse" title={labels.Close} onclick={close}>
+                    <svg class="slds-button__icon slds-button__icon_large" aria-hidden="true">
+                        <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#close"></use>
+                    </svg>
+                    <span class="slds-assistive-text">{labels.Close}</span>
+                </button>
+                <h2 id="modal-heading-01" class="slds-modal__title slds-hyphenate">{heading}</h2>
+            </header>`,
+      errors: [
+        { messageId: "removeClass" }
+      ],
+      output: `<header class="slds-modal__header">
+                <button class="slds-button slds-button_icon slds-modal__close" title={labels.Close} onclick={close}>
+                    <svg class="slds-button__icon slds-button__icon_large" aria-hidden="true">
+                        <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#close"></use>
+                    </svg>
+                    <span class="slds-assistive-text">{labels.Close}</span>
+                </button>
+                <h2 id="modal-heading-01" class="slds-modal__title slds-hyphenate">{heading}</h2>
+            </header>`,
+    }
+
   ],
 });
