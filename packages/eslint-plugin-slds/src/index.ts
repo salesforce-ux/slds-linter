489051e7cbcdd2617fb4bc4aca9100a28d9ab247
Kenam Verma <kenam.verma@salesforce.com>
2025-03-26 12:02:52 +0530

    fix: modify index file

--- packages/eslint-plugin-slds/src/index.ts
+++ packages/eslint-plugin-slds/src/index.ts
@@ -1,5 +1,4 @@
-"use strict";
-module.exports = {
+export = {
     rules: {
         "enforce-bem-class": require('./rules/enforce-bem-class'),
         "no-deprecated-classes-slds2": require('./rules/no-deprecated-classes-slds2'),
