67fb0aaa89b6c74d50ff868e7b3864205dd621a9
Naveen Ithappu <nithappu@salesforce.com>
2025-06-07 14:40:20 +0530

    fix: stop reporting --lwc tokens in fallback
    @W-18674330

--- packages/stylelint-plugin-slds/src/utils/decl-utils.ts
+++ packages/stylelint-plugin-slds/src/utils/decl-utils.ts
@@ -1,11 +1,15 @@
 import { Declaration } from "postcss";
 import valueParser from "postcss-value-parser";
 
+export function isVarFunction(node:valueParser.Node): boolean{
+  return (node.type === "function" && node.value === "var" && node.nodes.length>0);
+}
+
 export function forEachVarFunction(decl:Declaration, callback: (node: valueParser.Node, startOffset: number) => void, shallow: boolean = true) {
     const startOffset = decl.toString().indexOf(decl.value);
     const parsedValue = valueParser(decl.value);
     parsedValue.walk((node) => {
-      if (node.type === 'function' && node.value === 'var') {
+      if (isVarFunction(node)) {
         callback(node, startOffset);
         return !shallow;
       }
