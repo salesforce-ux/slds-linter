d0f9acc9f1c2ca842b59b5caf41b2b66b38f5fe0
Naveen Ithappu <nithappu@salesforce.com>
2025-06-20 17:38:24 +0530

    feat(stylelint): add no-slds-namespace rule to enforce token naming conventions
    - Introduced a new stylelint rule `slds/no-slds-namespace` to prevent the use of the `slds` namespace for custom tokens.
    - Updated rules metadata to include descriptions and severity levels for the new rule.
    - Added tests to validate the behavior of the new rule against various cases.
    
    This enhancement aims to ensure that custom tokens are clearly differentiated from SLDS tokens, promoting better naming practices in stylesheets.

---
+++ packages/stylelint-plugin-slds/tests/rules/no-slds-namespace/no-slds-namespace.spec.ts
@@ -0,0 +1,89 @@
+import stylelint, { LinterResult, LinterOptions } from 'stylelint';
+import sldsPlugin from '../../../src/index';
+
+const { lint }: typeof stylelint = stylelint;
+
+describe('no-slds-namespace', () => {
+  const testCases = [
+    {
+      message:
+        'Using slds namespace for --slds-my-own-token isn\'t supported. To differentiate SLDS and custom tokens, create a token in your namespace. Example: --myapp-my-own-token',
+      code: `
+        .example {
+          --slds-my-own-token: #fff;
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+    {
+      message:
+        'Using slds namespace for --slds-custom-color isn\'t supported. To differentiate SLDS and custom tokens, create a token in your namespace. Example: --myapp-custom-color',
+      code: `
+        .example {
+          color: var(--slds-custom-color);
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+    {
+      message: null,
+      code: `
+        .example {
+          --myapp-valid-token: #fff;
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+    {
+      message: null,
+      code: `
+        .example {
+          color: var(--myapp-valid-token);
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+    {
+      message: null,
+      code: `
+        .example {
+          --slds-c-button-color-background: #fff;
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+    {
+      message: null,
+      code: `
+        .example {
+          color: var(--slds-c-button-color-background);
+        }
+      `,
+      ruleName: 'slds/no-slds-namespace'
+    },
+  ];
+
+  testCases.forEach((testCase, index) => {
+    it(`test rule #${index}`, async () => {
+      const linterResult: LinterResult = await lint({
+        code: testCase.code,
+        config: {
+          plugins: [sldsPlugin],
+          rules: {
+            [testCase.ruleName]: true,
+          },
+        },
+      } as LinterOptions);
+
+      const messages = linterResult.results[0]._postcssResult?.messages || [];
+
+      // Test for the presence or absence of the message
+      if (testCase.message) {
+        expect(messages.length).toEqual(1);
+        expect(messages[0].text).toContain(testCase.message);
+      } else {
+        expect(messages.length).toEqual(0);
+      }
+    });
+  });
+}); 
