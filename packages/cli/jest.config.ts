499a0d49545699011b4207491ae940e70e14542d
Naveen Ithappu <nithappu@salesforce.com>
2025-06-27 11:46:18 +0530

    chore: update Jest configuration for ESM support
    - Enhanced Jest configuration in both cli and stylelint-plugin-slds packages to support ESM.
    - Added 'extensionsToTreatAsEsm' and 'transform' settings for TypeScript files.
    - Updated 'testMatch' patterns to include test files in the specified directories.

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -1,17 +1,27 @@
-// jest.config.mts
 import { createDefaultEsmPreset, type JestConfigWithTsJest } from 'ts-jest'
 
-const presetConfig = createDefaultEsmPreset({
-  tsconfig:"tsconfig.spec.json"
-})
-
 const jestConfig: JestConfigWithTsJest = {
-  ...presetConfig,
-  testPathIgnorePatterns:["build"],
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: "tsconfig.spec.json"
+    }],
+  },
+  testPathIgnorePatterns: ["build", "node_modules"],
+  testMatch: [
+    "**/tests/**/*.[jt]s?(x)",
+    "**/tests/**/?(*.)+(spec|test).[tj]s?(x)",
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
   coverageReporters: [
     "lcov",
     "json-summary"
   ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  roots: ['<rootDir>'],
 }
 
 export default jestConfig
