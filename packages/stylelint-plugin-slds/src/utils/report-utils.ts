2b87c2fc17f852dfcee8bc573f5a33e955822fe3
Ritesh Kumar <ritesh.kumar2@salesforce.com>
2025-07-03 20:53:44 +0530

    minor cleanup, working

--- packages/stylelint-plugin-slds/src/utils/report-utils.ts
+++ packages/stylelint-plugin-slds/src/utils/report-utils.ts
@@ -7,51 +7,6 @@ export interface MessagesObj {
   suggested: (oldValue: string) => string;
 }
 
-export function makeReportMatchingHooks(reportFn: Function) {
-  return function reportMatchingHooks(
-    valueNode: any,
-    suggestions: string[],
-    offsetIndex: number,
-    props: Partial<any>,
-    messages: MessagesObj,
-    fix?: Function
-  ) {
-    let index = offsetIndex;
-    const value = valueNode.value;
-    let endIndex = offsetIndex + valueNode.value.length;
-    const node = valueNode as any;
-    if ('sourceIndex' in valueNode && 'sourceEndIndex' in valueNode) {
-      index = valueNode.sourceIndex + offsetIndex;
-      endIndex = valueNode.sourceEndIndex + offsetIndex;
-    }
-    const reportProps = {
-      node,
-      index,
-      endIndex,
-      ...props,
-    };
-    if (suggestions.length > 0) {
-      reportFn({
-        message: JSON.stringify({
-          message: messages.rejected(value, generateSuggestionsList(suggestions)),
-          suggestions,
-        }),
-        ...reportProps,
-        fix: suggestions.length === 1 ? fix : null,
-      });
-    } else {
-      reportFn({
-        message: JSON.stringify({
-          message: messages.suggested(value),
-          suggestions: [],
-        }),
-        ...reportProps,
-      });
-    }
-  }
-}
-
-// Default for stylelint
 export function reportMatchingHooks(
   valueNode: any,
   suggestions: string[],
@@ -68,12 +23,14 @@ export function reportMatchingHooks(
     index = valueNode.sourceIndex + offsetIndex;
     endIndex = valueNode.sourceEndIndex + offsetIndex;
   }
+
   const reportProps = {
     node,
     index,
     endIndex,
     ...props,
   };
+
   if (suggestions.length > 0) {
     stylelint.utils.report(<stylelint.Problem>{
       message: JSON.stringify({
