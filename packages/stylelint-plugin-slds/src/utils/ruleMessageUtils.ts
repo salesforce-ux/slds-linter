362d24c0920259a2dd9c2b1c3a26f811c48f64aa
Ritesh Kumar <ritesh.kumar2@salesforce.com>
2025-07-22 15:43:27 +0530

    revert unnecessaryc hanges

--- packages/stylelint-plugin-slds/src/utils/ruleMessageUtils.ts
+++ packages/stylelint-plugin-slds/src/utils/ruleMessageUtils.ts
@@ -5,9 +5,7 @@ import { MessagesObj } from './reportUtils';
 export function toRuleMessages(ruleName: string, warningMsg: string): MessagesObj {
   return stylelint.utils.ruleMessages(ruleName, {
     rejected: (oldValue: string, newValue: string) =>
-      newValue && newValue.trim()
-        ? replacePlaceholders(warningMsg, { oldValue, newValue })
-        : `Replace the ${oldValue} static value: no replacement styling hook found.`,
+      replacePlaceholders(warningMsg, { oldValue, newValue }),
     suggested: (oldValue: string) =>
       `There's no replacement styling hook for the ${oldValue} static value. Remove the static value.`,
   });
