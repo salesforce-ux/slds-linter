9790a62bf2f68f75b60e6c9ccd16272b2862bfd9
Naveen Ithappu <nithappu@salesforce.com>
2025-07-31 11:24:32 +0530

    chore: corrected version suffix for internal final releases

--- scripts/release.js
+++ scripts/release.js
@@ -306,9 +306,9 @@ async function main() {
         {
           title: "Handle version generation",
           task: async (ctx) => {
-            ctx.finalVersion = ctx.version;
             const suffix = ctx.targetPersona === "external" ? "" : `-${ctx.targetPersona}`;
             const version = ctx.version + suffix;
+            ctx.finalVersion = version;
             if (ctx.releaseType !== "final") {
               ctx.finalVersion = await incrementPreReleaseVersion(
                 version,
