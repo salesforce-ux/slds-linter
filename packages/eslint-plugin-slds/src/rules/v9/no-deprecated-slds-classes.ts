0ba8f47f0d27d2d9eed76c69250065995d1ce5c8
Ritesh Kumar <ritesh.kumar2@salesforce.com>
2025-08-11 17:11:17 +0530

    No deprecated slds classes

---
+++ packages/eslint-plugin-slds/src/rules/v9/no-deprecated-slds-classes.ts
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview Rule to disallow deprecated SLDS CSS classes
+ * Compatible with @eslint/css parser for ESLint v9
+ * Maintains full parity with stylelint version behavior
+ */
+
+import { Rule } from 'eslint';
+import metadata from '@salesforce-ux/sds-metadata';
+import ruleMessages from '../../config/rule-messages.yml';
+
+const ruleConfig = ruleMessages['no-deprecated-slds-classes'];
+
+const deprecatedClasses = metadata.deprecatedClasses;
+const deprecatedClassesSet = new Set(deprecatedClasses);
+
+export default {
+  meta: {
+    type: ruleConfig.type,
+    docs: {
+      description: ruleConfig.description,
+      recommended: true,
+      url: ruleConfig.url,
+    },
+    fixable: null,
+    hasSuggestions: false,
+    schema: [],
+    messages: ruleConfig.messages,
+  },
+  
+  create(context) {
+    return {
+      // For no-deprecated-slds-classes: Check all class selectors for deprecated classes
+      "SelectorList Selector ClassSelector"(node) {
+        const cssClassSelector = context.sourceCode.getText(node);
+        
+        // Extract class name (remove the leading dot)
+        const className = cssClassSelector.substring(1);
+        
+        // Check if it's a deprecated SLDS class
+        if (className && deprecatedClassesSet.has(className)) {
+          context.report({
+            node,
+            messageId: 'deprecatedClass',
+            data: { className },
+          });
+        }
+      },
+    };
+  },
+} as Rule.RuleModule;
